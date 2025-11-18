760a273dba9054da10d33b0161818ec9e5c2f431
Laura Diaz <lasofidiaz484@gmail.com>
2025-11-17 23:40:18 -0500

    Entidad User

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Notification } from '../notification/entities/notification.entity';
+import { Historial } from '../historial/entities/historial.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User, Historial, Notification]),
+  ],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
